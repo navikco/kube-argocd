e166f0e845a115988b53c2ef7ac305a1b02b11f6
hpatel <hpatel@securustechnologies.com>
2020-10-07 12:05:02 -0500

    Added Gradle install in Dockerfile - 12...

--- setup/docker/build/bin/setupKUBEUser.sh
+++ setup/docker/build/bin/setupKUBEUser.sh
@@ -5,8 +5,9 @@ set -e
 echo "###Setup kubeadmin User###"
 
 #groupadd kube
+adduser -g kubeadmin -s /bin/sh kubeadmin -D
 
-useradd -ms /bin/bash kubeadmin
+#useradd -ms /bin/bash kubeadmin
 #usermod -G kube kubeadmin
 
 echo "p@ss10n" | passwd --stdin kubeadmin
